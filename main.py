6f86373355a77f8fed7d7dd9485483ea3a6d67f4
Sahej Bahl <bahlsahej@gmail.com>
2024-01-12 01:48:23 -0500

    arm angle calculation

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                        )
             
-            print(landmarks)
         except:
             pass
 
