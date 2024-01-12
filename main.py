f9243b9066a44a30317770fc4f9834c801eb4a86
Sahej Bahl <bahlsahej@gmail.com>
2024-01-12 01:37:32 -0500

    Get Angle

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils #gives drawing utilities
 mp_pose = mp.solutions.pose #imports pose estimation model from mp 
 
+#function to find angle
+def calculate_angle(a,b,c): 
+    a = np.array(a) #first
+    b = np.array(b) #mid
+    c = np.array(c) #end
+
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0]) #caclulates radians
+    angle = np.abs(radians*180.0/np.pi) #calculates angle
+
+    if angle > 180.0: #max 180 degrees
+        angle = 360-angle
+    
+    return angle 
+
 cap = cv2.VideoCapture(0) #set video capture device
 #setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -19,6 +33,28 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
         image.flags.writeable = True #set writable status to true
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) #recolor back to BGR
+
+        #Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+
+            #get coordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y] #grabs x and y value
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y] #grabs x and y value
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y] #grabs x and y value
+
+            #calculate angle
+            angle = calculate_angle(shoulder, elbow, wrist)
+
+            #visualize
+            cv2.putText(image, str(angle),
+                        tuple(np.multiply(elbow, [640, 480]).astype(int),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AAA)
+                       )
+            
+            print(landmarks)
+        except:
+            pass
 
         #render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
